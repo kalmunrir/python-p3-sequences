7cc5dcbf382e1d6212b9726a4049f59d987a47be
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-09-12 12:21:47 -0600

    finished lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    if length >= 1:
+        fib.append(0)
+    if length >= 2:
+        fib.append(1)
+    if length >= 3:
+        for i in range(length-2):
+            fib.append(fib[len(fib) - 1] + fib[len(fib) - 2])
+            
+    print(fib)
